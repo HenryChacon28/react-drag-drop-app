e28e1d5e8baea60cbaacb263ad9dd67b038172bd
Henry <Henrychaconrm@gmail.com>
2021-07-27 19:04:03 -0600

    Primer commit para subir repositorio

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import "./App.css";
+
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const initialTasks = [
+  {
+    id: "1",
+    text: "React",
+  },
+  {
+    id: "2",
+    text: "Angular",
+  },
+  {
+    id: "3",
+    text: "Vue",
+  },
+  {
+    id: "4",
+    text: "Svelte",
+  },
+];
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = [...list];
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  return (
+    <DragDropContext
+      onDragEnd={(result) => {
+        const { source, destination } = result;
+        if (!destination) {
+          return;
+        }
+        if (
+          source.index === destination.index &&
+          source.droppableId === destination.droppableId
+        ) {
+          return;
+        }
+
+        setTasks((prevTasks) =>
+          reorder(prevTasks, source.index, destination.index)
+        );
+      }}
+    >
+      <div className="app">
+        <h1>El Mejor Framework De JavaScript</h1>
+        <Droppable droppableId="tasks">
+          {(droppableProvided) => (
+            <ul
+              {...droppableProvided.droppableProps}
+              ref={droppableProvided.innerRef}
+              className="task-container"
+            >
+              {tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(draggableProvided) => (
+                    <li
+                      {...draggableProvided.draggableProps}
+                      ref={draggableProvided.innerRef}
+                      {...draggableProvided.dragHandleProps}
+                      className="task-item"
+                    >
+                      {task.text}
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {droppableProvided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+}
+
+export default App;
